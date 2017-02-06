75e29d8f315dd0eb4d12b0009a30561c9ed7904e
Yanni Coroneos <spock@csail.mit.edu>
2017-02-06 12:34:28 -0500

    Even though this project has the same topic as Biscuit, its implementation and target use has diverged significantly. Renaming to GERT (Go Embedded RunTime).

--- gert/armv7a/measurements/counter/irq.go
+++ gert/armv7a/measurements/counter/irq.go
@@ -3,7 +3,7 @@ package main
 import "./embedded"
 
 /*
-* This is the interrupt handler for all IRQs in the biscuit embedded system.
+* This is the interrupt handler for all IRQs in GERT.
 * It is extremely important that nothing in this code causes the scheduler to run
 * or trigger a garbage collection. This is because this code may run while locks are held
 * or even when the garbage collector is running too. This runs with CPSR=IRQ mode.
